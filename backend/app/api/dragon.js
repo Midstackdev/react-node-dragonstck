ba9d25278fcd48fb9b1da42db393a178d4922b1d
Midstackdev <midstackdev@gmail.com>
2021-11-07 00:53:25 +0000

    buy dragon

--- backend/app/api/dragon.js
+++ backend/app/api/dragon.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import AccountTable from '../account/table.js';
 import AccountDragonTable from '../accountDragon/table.js';
 import { getPublicDragons } from '../dragon/helper.js';
 import DragonTable from '../dragon/table.js';
@@ -37,6 +38,61 @@ router.put('/update', (req, res, next) => {
 router.get('/public', (req, res, next) => {
     getPublicDragons()
         .then(({ dragons }) => res.json({ dragons }))
+        .catch(error => next(error));
+});
+
+router.post('/buy', (req, res, next) => {
+    const { dragonId, saleValue } = req.body;
+    let buyerId;
+
+    DragonTable.getDragon({ dragonId })
+        .then(dragon => {
+            if(dragon.saleValue !== saleValue) {
+                throw new Error('Sale value is not correct');
+            }
+            
+            if(!dragon.isPublic) {
+                throw new Error('Dragon must be public');
+            }
+
+            return authenticateAccount({ sessionString: req.cookies.sessionString });
+        })
+        .then(({ account, authenticated }) => {
+            if(!authenticated) {
+                throw new Error('Unauthenticated');
+            }
+            
+            if(saleValue > account.balance) {
+                throw new Error('Sale value exceeds balance');
+            }
+
+            buyerId = account.id;
+
+            return AccountDragonTable.getDragonAccount({ dragonId });
+        })
+        .then(({ accountId }) => {
+            if(accountId === buyerId) {
+                throw new Error('Cannot buy your own dragon!');
+            }
+
+            const sellerId = accountId;
+
+            return Promise.all([
+                AccountTable.updateBalance({
+                    accountId: sellerId, value: saleValue
+                }),
+                AccountTable.updateBalance({
+                    accountId: buyerId, value: -saleValue
+                }),
+                AccountDragonTable.updateDragonAccount({
+                    dragonId, accountId: buyerId
+                }),
+                DragonTable.updateDragon({
+                    dragonId, isPublic: false
+                })
+            ])
+        })
+        .then(() => res.json({ message: 'success!' }))
         .catch(error => next(error));
 });
 
