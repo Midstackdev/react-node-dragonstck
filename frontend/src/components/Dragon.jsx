595615caf72b6e05ca4b53f9892133a6975f8e4c
Midstackdev <midstackdev@gmail.com>
2021-11-11 21:08:12 +0000

    limit dragon per user per generation

--- frontend/src/components/Dragon.jsx
+++ frontend/src/components/Dragon.jsx
@@ -1,21 +1,31 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchDragon } from "../actions/dragonActions";
+import fetchStates from "../reducers/fetchStates";
 import DragonAvatar from "./DragonAvatar";
 
 
 const Dragon = () => {
     const dispatch = useDispatch();
-    const dragon = useSelector(state => state.dragon)
+    const dragon = useSelector(state => state.dragon);
+    const { status, message } = dragon;
 
     // useEffect(() => {
     //     dispatch(fetchDragon())
     // }, [dispatch])
 
+    const dragonView = () => {
+        if(status === fetchStates.error) {
+            return (<span className="text-danger">{message}</span>)
+        }
+        return (<DragonAvatar dragon={dragon} />)
+    }
+
     return dragon && (
         <div>
             <button className="btn btn-dark" onClick={() => dispatch(fetchDragon())}>New Dragon</button>
-            <DragonAvatar dragon={dragon} />
+            <br />
+            {dragonView()}
         </div>
     )
 }
